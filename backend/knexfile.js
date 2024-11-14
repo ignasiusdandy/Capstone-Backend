ace5cb8f648c6c28646d18f8fb5a08c54da5af60
ignasiusdandy <ignasiusdandy123@gmail.com>
2024-11-14 17:23:27 +0800

    docker compose

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -8,7 +8,7 @@ module.exports = {
   development: {
     client: 'mysql_db',
     connection: {
-      host: mysql_db,
+      host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       port: process.env.PORT,
